018c2f450d98a64e3bfb0e536e39ca912184d6c4
Benjamin <ben@ayles.co.nz>
2016-09-10 12:47:46 +0930

    still testing

--- lib/led.py
+++ lib/led.py
@@ -6,22 +6,27 @@ class Led(object):
         self.active = False
 
     def activate(self):
-        self.__toggle(True)
+        self.__toggle(1)
         self.active = True
 
     def deactivate(self):
-        self.__toggle(False)
+        self.__toggle(0)
         self.active = False
 
     def __toggle(self, state):
         state_list = list(bin(self.active)[2:].zfill(8))
 
         print(state)
-        print(int(state))
-        print(str(int(state)))
+        print(state_list)
 
-        state_list[self.port] = str(int(state))
+        state_list[self.port] = state
         state = ''.join(state_list)
 
+        print(state_list)
+        print(state)
+
         state = int(state, 2)
+
+        print(state)
+
         self.bus.write_byte(self.address, state)
